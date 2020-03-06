e7f1f24a02d29e9206f697edcbcc6e39bde3497d
huuyafwww <huuya1234fwww@gmail.com>
2020-03-07 05:12:38 +0900

    Delete Database Relation Class,Because Add illuminate/database

--- develovation/app/core/init/class/__Model.php
+++ develovation/app/core/init/class/__Model.php
@@ -6,12 +6,98 @@
 class __Model{
 
     /**
+     * This is Array Uri
+     *
+     * @access protected
+     * @var DB
+     */
+    protected $db;
+
+    /**
      * Model Init
      * 
      * @access protected
      */
     protected function __construct()
     {
+        // Auto Loader
+        $this->__auto_loader();
+    }
+
+    /**
+     * Auto Loader
+     * 
+     * @access private
+     */
+    private function __auto_loader()
+    {
+        // "illuminate/database" Class to DB Class
+        $this->__create_alias();
+
+        // Get Databese Class
+        $this->__get_db_class();
+
+        // Connect Database
+        $this->__connect_db();
+
+        // Set Global for DB Class
+        $this->__set_global();
+
+        // Enable Eloquent
+        $this->__enable_eloquent();
+    }
+
+    /**
+     * "illuminate/database" Class to DB Class
+     * 
+     * @access private
+     */
+    private function __create_alias()
+    {
+        class_alias(
+            Illuminate\Database\Capsule\Manager::class,
+            ILLUMINATE_DB_ALIAS_NAME
+        );
+    }
+
+    /**
+     * Get Databese Class
+     * 
+     * @access private
+     */
+    private function __get_db_class()
+    {
+        $this->db = new DB();
+    }
+
+    /**
+     * Connect Database
+     * 
+     * @access private
+     */
+    private function __connect_db()
+    {
+        $this->db->addConnection(DB_CONFIG);
+    }
+
+    /**
+     * Set Global for DB Class
+     * 
+     * @access private
+     */
+    private function __set_global()
+    {
+        $this->db->setAsGlobal();
+    }
+
+    /**
+     * Enable Eloquent
+     * 
+     * @access private
+     */
+    private function __enable_eloquent()
+    {
+        $this->db->bootEloquent();
     }
 
 }
--- develovation/app/core/init/config/__db.php
+++ develovation/app/core/init/config/__db.php
@@ -16,6 +16,23 @@ define("DB_USER","root");
 // DataBase Password
 define("DB_PASSWORD","root");
 
+// "illuminate/database" Alias Name
+define("ILLUMINATE_DB_ALIAS_NAME","DB");
+
+// DataBase Config
+define(
+    "DB_CONFIG",
+    [
+        "driver" => 'mysql',
+        "host" => DB_HOST,
+        "database" => DB_NAME,
+        "username" => DB_USER,
+        "password" => DB_PASSWORD,
+        "charset" => 'utf8mb4',
+        "collation" => 'utf8mb4_general_ci',
+    ]
+);
+
 /**
  * /Now ORM Wrapper Developing Debug Info
  */
