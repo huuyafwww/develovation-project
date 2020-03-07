0ff0e302e0156fc1788ec0b5901b46e7b238516a
huuyafwww <huuy1234fwww@gmail.com>
2020-03-07 23:41:00 +0900

    Create Login Page

---
+++ develovation/app/models/auth/login.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * View Model
+ */
+class __M_Login extends __Model{
+
+    /**
+     * Model Init
+     */
+    public function __construct()
+    {
+        // Run Parent Class Constructor
+        parent::__construct();
+    }
+
+    /**
+     * This is a Model Run Sample Code
+     */
+    public function __test()
+    {
+        echo "Testing";
+    }
+
+}
