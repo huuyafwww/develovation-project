c867f0393987e9b25805bad4a20c9cb237ac325d
huuyafwww <huuya1234fwww@gmail.com>
2020-04-19 17:51:54 +0900

    Fixed .gitignore

--- temp/stisla-php-ver/parts/nav-menu.php
+++
@@ -1,8 +0,0 @@
-<!-- Nav-Menu-Message -->
-<?php __get_component("nav-menu-message.php"); ?><!-- /Nav-Menu-Message -->
-
-<!-- Nav-Menu-Notification -->
-<?php __get_component("nav-menu-notification.php"); ?><!-- /Nav-Menu-Notification -->
-
-<!-- Nav-Menu-User -->
-<?php __get_component("nav-menu-user.php"); ?><!-- /Nav-Menu-User -->
