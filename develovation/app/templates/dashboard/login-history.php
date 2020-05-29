db00b0d0c87035e99feaa2ccebc72fb3820fba1c
huuyafwww <huuy1234fwww@gmail.com>
2020-05-29 22:10:21 +0900

    Add Sample Login History Page

---
+++ develovation/app/templates/dashboard/login-history.php
@@ -0,0 +1,40 @@
+<div class="row">
+    <div class="col-12">
+
+        <!-- Card -->
+        <div class="card">
+
+            <!-- Card-Body -->
+            <div class="card-body p-0">
+                <table class="table table-striped table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">項番</th>
+                            <th scope="col">IPアドレス</th>
+                            <th scope="col">ログイン日時</th>
+                            <th scope="col">デバイス</th>
+                            <th scope="col">種類</th>
+                            <th scope="col">OS</th>
+                            <th scope="col">ブラウザ名</th>
+                            <th scope="col">ブラウザVer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div><!-- /Card-Body -->
+
+        </div><!-- /Card -->
+
+    </div>
+</div>
